e408a13e504015eb6741908a0ffdbf0acb85935a
Dustin <dzubke@outlook.com>
2021-03-24 20:29:46 +0000

    updates to dockerfile and kubernetes files

--- kubernetes/scripts/pod_stats.py
+++ kubernetes/scripts/pod_stats.py
@@ -3,16 +3,18 @@ import os
 import time
 # third-party libs
 import torch
+from warpctc_pytorch import CTCLoss
 
 
 if __name__ == "__main__":
 
-	while True:
-		print(f"data dir /mnt/disks/data_disk/ exists: {os.path.exists('/mnt/disks/data_disk')}")
-		print(f"data dir /mnt/disks/data_disk/data/ exists: {os.path.exists('/mnt/disks/data_disk/data')}")
-		print(f"gpu available: {torch.cuda.is_available()}")
-		with open("/tmp/log.txt", 'w') as fid:
-			fid.write(f"data dir /mnt/disks/data_disk/ exists: {os.path.exists('/mnt/disks/data_disk')}\n")
-			fid.write(f"data dir /mnt/disks/data_disk/data/ exists: {os.path.exists('/mnt/disks/data_disk/data')}\n")
-			fid.write(f"gpu available: {torch.cuda.is_available()}\n")
-		time.sleep(1)
+    while True:
+        print(f"data dir /mnt/disks/data_disk/ exists: {os.path.exists('/mnt/disks/data_disk')}")
+        print(f"data dir /mnt/disks/data_disk/data/ exists: {os.path.exists('/mnt/disks/data_disk/data')}")
+        print(f"gpu available: {torch.cuda.is_available()}")
+        print(f"type of naren CTC Loss: {type(CTCLoss)}")
+        with open("/tmp/log.txt", 'w') as fid:
+            fid.write(f"data dir /mnt/disks/data_disk/ exists: {os.path.exists('/mnt/disks/data_disk')}\n")
+            fid.write(f"data dir /mnt/disks/data_disk/data/ exists: {os.path.exists('/mnt/disks/data_disk/data')}\n")
+            fid.write(f"gpu available: {torch.cuda.is_available()}\n")
+        time.sleep(1)
